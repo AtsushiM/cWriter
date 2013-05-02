d546538bf0474a808a7186e219ea63178df496e4
AtsushiM <asionfb@gmail.com>
2013-05-02 19:43:37 +0900

    Helper.chkAgent cached.

--- src/_helper.js
+++ src/_helper.js
@@ -1,11 +1,14 @@
 CW.prototype.Helper = {
     _UA: window.navigator.userAgent.toLowerCase(),
+    _ret: null,
     _checkBrowser: function(browsernames) {
         var i,
             len;
 
         for (i = 0, len = browsernames.length; i < len; i++) {
             if (this._UA.indexOf(browsernames[i]) !== -1) {
+                this._ret = browsername;
+
                 return browsername;
             }
         }
@@ -13,6 +16,10 @@ CW.prototype.Helper = {
         return false;
     },
     chkAgent: function() {
+        if (this._ret !== null) {
+            return this._ret;
+        }
+
         return this._checkBrowser([
             'chrome',
             'firefox',
